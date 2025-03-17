600543369422f39faed8a9a3bd96a46f88751def
EsethuSovendle <esovendle@gmail.com>
2025-03-17 11:13:51 +0200

    Add files via upload

---
+++ Prjclass1.java
@@ -0,0 +1,24 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclass1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Prjclass1 {
+
+    public static void main(String[] args) {
+        
+        private static student s = new students(); // object of our student class
+        
+        public static void main(String[] args) {
+            
+        String strName = JOptionPane.showInputDialog(null, "Please enter student name");
+        String strCourse = JOptionPane.showInputDialog(null, "Please enter student course");
+        double dblAvg = Double.parseDouble(JOptionPane.showI)
+    }
+}
